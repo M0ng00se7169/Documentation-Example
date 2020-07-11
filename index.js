ae582815d4e387cb28aae580ac7efd03d4c3517d
M0ng00se7169 <NickKomarowski@gmail.com>
2020-07-11 16:12:28 +0300

    Added express

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const path = require('path')
+const express = require('express')
 const port = 8000;
+
+// здесь у нас происходит импорт пакетов и определяется порт нашего сервера
+const app = express();
+
+//здесь наше приложение отдаёт статику
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
+
+console.log(port);
 
 app.listen(port);
